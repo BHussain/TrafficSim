f2e1204506a2833da9d2df5c4f22f246ed9fec99
BHussain <b_hussain@hotmail.nl>
2016-04-07 09:45:58 +0200

    Initialized repository

---
+++ ConsoleApplication1/TrafficSimulation.fs
@@ -0,0 +1,50 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+module TrafficSimulation
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Input
+
+type Color = 
+    | Red of float32
+    | Green of float32
+
+type trafficLight =
+    {
+        Color : Color
+        Position : int*int
+    }
+
+type gameState =
+    {
+        trafficLight : trafficLight
+    }
+
+let initialState()=
+    {
+        trafficLight = 
+        {
+            Color = Red(3.0f)
+            Position = 20,20
+         }
+    }
+
+let updateTrafficLight (trafficLight:trafficLight) (dt:float32) : trafficLight =
+    let newColor = 
+        match trafficLight.Color with
+        | Red t ->
+            if t <= 0.0f then
+                Green(1.0f)
+            else
+                Red(t - dt)
+        | Green t ->
+            if t <= 0.0f then
+                Red(3.0f)
+            else
+                Green(t - dt)
+    { trafficLight with Color = newColor }
+    
+let updateState (dt:float32) (gameState:gameState) =
+    {
+        gameState with trafficLight = updateTrafficLight gameState.trafficLight dt
+    }
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace ConsoleApplication2
+{
+    class Game : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        TrafficSimulation.gameState gameState;
+
+        public Game()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void LoadContent()
+        {
+            gameState = TrafficSimulation.initialState();
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            gameState = TrafficSimulation.updateState(
+              (float)gameTime.ElapsedGameTime.TotalSeconds,
+              gameState);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            //Print de huidige waarde van de stoplicht float
+            Console.WriteLine(gameState.trafficLight.Color);
+            base.Draw(gameTime);
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
