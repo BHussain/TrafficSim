949e84f43c612c5f1c6111ee3f3dbb60821511ba
BHussain <b_hussain@hotmail.nl>
2016-04-18 14:41:40 +0200

    Spawning in intervals implemented

--- ConsoleApplication1/TrafficSimulation.fs
+++ ConsoleApplication1/TrafficSimulation.fs
@@ -3,7 +3,9 @@
 open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Input
 
-let r = System.Random()
+type Spawner =
+    | Cooldown of float32
+    | Ready
 
 type List<'a> = 
   | Empty 
@@ -47,72 +49,116 @@ type TrafficLight =
         Position : float32*float32
     }
 
-type MoveableType = 
-    | Child
-    | Car
+//type MoveableType = 
+//    | Child
+//    | Car
 
-type Moveable = 
+type Child =
     {
         Position        : float32*float32
         Waiting         : bool
-        MoveableType    : MoveableType
+        Velocity        : float32
+    }
+
+type Car =
+    {
+        Position        : float32*float32
+        Waiting         : bool
+        Velocity        : float32
     }
 
 type GameState =
     {
+        carSpawner : Spawner
+        childSpawner : Spawner
         TrafficLight : TrafficLight
-        Moveables : List<Moveable>
+        cars : List<Car>
+        children : List<Child>
     }
 
 let InitialState()=
     {
-        Moveables = Empty
+        carSpawner = Spawner.Ready
+        childSpawner = Spawner.Ready
         TrafficLight = 
             {
                 Color = Red(3.0f)
                 Position = 400.0f, 220.0f
             }
+        cars = Empty
+        children = Empty
     }
 
-let randomMoveable() =
+let randomCar() =
   {
-    Moveable.Position = (10.0f, 150.0f)
-    Moveable.Waiting = false;
-    Moveable.MoveableType = Car
+    Car.Position = (10.0f, 150.0f)
+    Car.Waiting = false;
+    Car.Velocity = 3.0f;
   }
 
-let Walking (child:Moveable) (dt:float32) : Moveable =
+let randomChild() =
+  {
+    Child.Position = (500.0f, 400.0f)
+    Child.Waiting = false;
+    Child.Velocity = 1.0f;
+  }
+
+let updateChild  (dt:float32) (child:Child): Child =
     {
-        child with Position = (fst(child.Position), snd(child.Position) - 1.0f)
+        child with Position = (fst(child.Position), snd(child.Position) - child.Velocity)
     }
 
-let Driving (car:Moveable) (dt:float32) : Moveable =
+let updateCar (dt:float32) (car:Car) : Car =
     {
-        car with Position = (fst(car.Position) + 3.0f), snd(car.Position)
+        car with Position = (fst(car.Position) + car.Velocity), snd(car.Position)
     }
 
-let UpdateMoveable (dt:float32) (moveable:Moveable) =
-    match moveable.MoveableType with
-    | Car -> Driving moveable dt
-    | Child -> Walking moveable dt
+let updateSpawner (spawner:Spawner)(dt:float32) :Spawner =
+        let r = new System.Random()
+        match spawner with
+        | Ready ->
+            spawner
+        | Cooldown t ->
+            if t > 0.0f then
+                Cooldown(t-dt)
+            else
+                Ready
+    
 
-let updateMoveables (dt:float32) (moveables:List<Moveable>) : List<Moveable> =
-//  let creationProbability =
-//    let l = length moveables
-//    if l < 5 then
-//      100
-//    elif l < 15 then
-//      20
-//    else
-//      5
-  let moveables = 
-    //let r = new System.Random()
-    if  r.Next(0,100) < 2 then
-      randomMoveable() << moveables
-    else
-      moveables
-  let moveables = map (UpdateMoveable dt) moveables
-  moveables
+let updateCars (dt:float32) (cars:List<Car>)(spawnCar:bool) : List<Car> =
+  let cars = 
+        if  spawnCar = true then
+            randomCar() << cars
+        else
+        cars
+  let cars = map (updateCar dt) cars
+  cars
+
+let updateChildren (dt:float32) (children:List<Child>)(spawnChild:bool) : List<Child> =
+  let children = 
+        if  spawnChild = true then
+            randomChild() << children
+        else
+        children
+  let children = map (updateChild dt) children
+  children
+
+//let updateList (dt:float32) (list:List<'a>)(canSpawn:bool) : List<'a> =
+//  let children = 
+//        if  canSpawn = true then
+//            match list with
+//            | Empty -> Empty
+//            | Node(x:'a,xs:List<'a>) ->
+//                match x.GetType with
+//                | Child ->
+//                    randomChild() << list
+//                | Car ->
+//                    randomCar() << list
+//            randomChild() << children
+//        else
+//        children
+//  let children = map (updateChild dt) children
+//  children 
 
 let UpdateTrafficLight (trafficLight:TrafficLight) (dt:float32) : TrafficLight =
     let newColor = 
@@ -130,9 +176,33 @@ let UpdateTrafficLight (trafficLight:TrafficLight) (dt:float32) : TrafficLight =
     { trafficLight with Color = newColor }
    
 let UpdateState (dt:float32) (gameState:GameState) =
+    
+    let spawnCar,newCarSpawner = 
+        match gameState.carSpawner with
+            | Ready ->
+                    true, Cooldown 2.0f
+            | Cooldown t ->
+                if t > 0.0f then
+                    false, Cooldown(t-dt)
+                else
+                    false, Ready
+
+    let spawnChild, newChildSpawner =
+        match gameState.carSpawner with
+            | Ready ->
+                    true, Cooldown 1.0f
+            | Cooldown t ->
+                if t > 0.0f then
+                    false, Cooldown(t-dt)
+                else
+                    false, Ready
+
     {
-        gameState with TrafficLight = UpdateTrafficLight gameState.TrafficLight dt
-                       Moveables = updateMoveables dt gameState.Moveables
+        gameState with  carSpawner = newCarSpawner
+                        childSpawner = newChildSpawner
+                        TrafficLight = UpdateTrafficLight gameState.TrafficLight dt
+                        cars = updateCars dt gameState.cars spawnCar
+                        children = updateChildren dt gameState.children spawnChild
     }
 
 type Drawable = 
@@ -141,15 +211,23 @@ type Drawable =
     Image    : string
   }
 
- let drawMoveables (m:Moveable) : Drawable =
+ let drawCars (car:Car) : Drawable =
   {
-    Drawable.Position = m.Position
+    Drawable.Position = car.Position
     Drawable.Image    = "car_red.png"
   }
 
-let drawState (gameState:GameState) : seq<Drawable> =
-  let listOfDrawableMoveables =
-    map drawMoveables gameState.Moveables |> toFSharpList
+ let drawChildren (child:Child) : Drawable =
+   {
+    Drawable.Position = child.Position
+    Drawable.Image    = "character_black_blue.png"
+   }
+
+ let drawState (gameState:GameState) : seq<Drawable> =
+  let listOfDrawableCars =
+    map drawCars gameState.cars |> toFSharpList
+  let listOfDrawableChildren =
+    map drawChildren gameState.children |> toFSharpList
   [
     {
       Drawable.Position = gameState.TrafficLight.Position
@@ -157,7 +235,7 @@ let drawState (gameState:GameState) : seq<Drawable> =
                           | Red t -> "red.png"
                           | Green t -> "green.png"
     }
-  ] @ listOfDrawableMoveables
+  ] @ listOfDrawableCars @listOfDrawableChildren
     |> Seq.ofList
 
 //let drawState (gameState:GameState) : seq<Drawable> =
